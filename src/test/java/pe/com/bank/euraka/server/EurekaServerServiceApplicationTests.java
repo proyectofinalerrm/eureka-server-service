a14fa830d8286f2e84d9b4997656ff4a86157900
Edwin Ruben Rea Morales <ereamora@emeal.nttdata.com>
2022-05-31 12:21:24 -0500

    first commit eureka server service

---
+++ src/test/java/pe/com/bank/euraka/server/EurekaServerServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package pe.com.bank.euraka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class EurekaServerServiceApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
